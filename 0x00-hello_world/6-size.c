a76b2ab15f9ef7c3ce35d278f2704fe4d3603cca
Daisy <chepkemoi796@gmail.com>
2023-03-16 13:32:31 -0700

    size of specifiers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of int:%d", size of(int)\n);
+printf("size of char:%c", size of(char)\n);
+printf("size of long int:%ld", size of(long int)\n);
+printf("size of long long int:%lid", size of(long long int)\n);
+printf("size of float:%f", size of(float)\n);
+
+	return (0);
+}
