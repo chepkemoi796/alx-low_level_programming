dd588ed1be85083fb41c6ff0f2080063dbb90b81
Daisy <chepkemoi796@gmail.com>
2023-03-20 04:34:58 -0700

    possible combinations of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,5 +7,14 @@
  */
 int main(void)
 {
-
+int num;
+for ((num = 1); (num < 11); num++)
+{
+putchar((num % 10) + '0');
+}
+if (num != 10)
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
 }
