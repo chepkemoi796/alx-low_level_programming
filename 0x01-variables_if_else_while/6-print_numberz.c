b3ae196844df8d1f028410038b845a8cae5b104e
Daisy <chepkemoi796@gmail.com>
2023-03-20 02:34:50 -0700

    prints digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: (0) Always (Success)
+ *
+ */
+int main(void)
+{
+int num;
+for ((num = 0); (num < 10); num++)
+{
+putchar((num % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
