e1e39f657b136634444c23b399878e387ff7f209
Daisy <chepkemoi796@gmail.com>
2023-03-20 02:02:41 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: (0) Always (Success)
+ *
+ */
+int main(void)
+{
+int num;
+for ((num = 0); (num < 10); num++)
+{
+printf("%d", num);
+}
+return (0);
+}
