4e0427885bd57bfc343162b65b6a02282c4c3183
Daisy <chepkemoi796@gmail.com>
2023-03-20 09:50:14 -0700

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: (0) Always (Success)
+ *
  */
 int main(void)
 {
-printf("size of int:%d", size of(int)\n);
-printf("size of char:%c", size of(char)\n);
-printf("size of long int:%ld", size of(long int)\n);
-printf("size of long long int:%lid", size of(long long int)\n);
-printf("size of float:%f", size of(float)\n);
-
-	return (0);
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("size a of char: %d\n", c);
+printf("size a of int: %d\n", i);
+printf("size a of long int: %d\n", li);
+printf("size a of long long int: %d\n", lli);
+printf("size a of float: %d\n", f);
+return (0);
 }
