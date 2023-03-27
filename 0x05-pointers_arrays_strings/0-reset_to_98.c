efb057b3fa374f46ca92b8222ae8c3e3aaee7ab3
Daisy <chepkemoi796@gmail.com>
2023-03-27 10:47:46 -0700

    reseting variable to different value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 -  function that takes a pointer to an int as parameter
+ * @*n: a pointer that points to integer variable value
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h_
+#define main_h_
+
+int _putchar(char);
+void reset_to_98(int *n);
+#endif
