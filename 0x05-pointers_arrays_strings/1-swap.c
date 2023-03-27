5ef031083bcabc1d65daa1280d5f899f74b519dc
Daisy <chepkemoi796@gmail.com>
2023-03-27 11:13:23 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integers
+ * @*a: pointer for a
+ * @b: pointer for b
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - prints length of string
+ * @*s: pointer 
+ */
+int _strlen(char *s)
+{
+}
