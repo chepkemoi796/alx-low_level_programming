7e0b5469eaa3c72398815902d368a6a8aba37a77
Daisy <chepkemoi796@gmail.com>
2023-04-10 05:37:50 -0700

    main arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,17 +3,12 @@
 /**
  * main - entry point
  * @argc: number of arguments
- * @argv[]: list of arguments
+ * @argv: list of arguments
  * @__attribute__((unused)): compile unused functions and variables
- * @count: index
  * Return: (0)
  */
-int main(int argc, char* argv[]__attribute__((unused))) 
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-int count;
-for ( count = 0; count < argc; count++)
-{
-printf("%d\n", argc);
-}
+printf("%d\n", argc - 1);
 return (0);
 }
