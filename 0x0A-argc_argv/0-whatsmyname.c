89c11e230e2823a00f77c0f2ecde315015e527a1
Daisy <chepkemoi796@gmail.com>
2023-04-10 03:05:56 -0700

    main arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
 int main(int argc, char *argv[])
 {
 int count;
-for (count = 0; count< argc; count++)
+for (count = 0; count < argc; count++)
 {
-printf("%s\n", argv[count]);
+printf("%s\n", argv[0]);
 }
 return (0);
 }
