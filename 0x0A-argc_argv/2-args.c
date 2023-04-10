9a2b7428ba2856b6401b127397210e49eb6e5bb9
Daisy <chepkemoi796@gmail.com>
2023-04-10 05:46:39 -0700

    argv and argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,10 @@
 /**
  * main - entry point
  * @argc: argument count
- * @argv[]: argument list
- * @count: index
+ * @argv: argument list
  * Return: (0)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 int count;
 for (count = 0; count < argc; count++)
