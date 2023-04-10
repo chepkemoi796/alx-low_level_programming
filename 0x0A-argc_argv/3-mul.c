6d762ed0c9c3f53b00f75bf46b5e258f24285e19
Daisy <chepkemoi796@gmail.com>
2023-04-10 05:52:37 -0700

    main arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,14 +3,13 @@
 /**
  * main - entry point
  * @argc: argument count
- * @argv[]: argument list
- * @mul: multiplication variable
+ * @argv: argument list
  * Return: (0)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 int mul;
-if ( argc < 2)
+if (argc < 2)
 {
 printf("Error\n");
 return (1);
