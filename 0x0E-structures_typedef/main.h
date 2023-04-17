65df271e096cde85aa26b2564b2783de9245d4f0
Daisy <chepkemoi796@gmail.com>
2023-04-17 09:16:06 -0700

    structures

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h_
+#define main_h_
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
