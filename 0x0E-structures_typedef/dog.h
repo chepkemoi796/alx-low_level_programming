ff9fa5e61abe88f2104cf91a2269fb4ccbd1ef6b
Daisy <chepkemoi796@gmail.com>
2023-04-18 10:44:33 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,8 +7,16 @@ char *name;
 float age;
 char *owner;
 };
+#endif
+#ifndef DOG_H_
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
+#ifndef DOG_H_
 void print_dog(struct dog *d);
+#endif
+#ifndef DOG_H_
 typedef struct dog dog_t;
+#endif
+#ifndef DOG_H_
 void free_dog(dog_t *d);
 #endif
