477544442900f399818d08c539f96c5ed3312ffc
Daisy <chepkemoi796@gmail.com>
2023-04-18 10:35:26 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: type of variable
+ */
 void print_dog(struct dog *d)
 {
 if (d->owner == NULL)
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,9 @@
 #include <stdioh>
 #include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: to be freed
+ */
 void free_dog(dog_t *d)
 {
 if (d)
