95aa2b7ebd56aafbe08f84c454df84dad0de1952
daisy <chepkemoi796@gmail.com>
2023-06-11 06:48:42 -0700

    argc, argv[]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,12 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc <= 0)
+int i;
+int sum = 0;
+if (argc == 0)
+{
 printf("0\n");
+}
 for (i = 0; i < argc; i++)
 {
-
+if (atoi(argv[i]) < '0' && atoi(argv[i]) > '9')
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+sum += atoi(argv[i]);
 }
 }
 printf("%d\n", sum);
